8af8e82b85020e17ca6f26ceeb059ee89081ac87
Alexander Kromm <mmaulwurff@gmail.com>
2020-08-04 20:57:52 +0700

    add description to build.sh and fix shellcheck warnings

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# This script packages Warm Reception.
+#
+# Usage: ./scripts/build.sh
+
 set -e
 
 filename=warm-reception-$(git describe --abbrev=0 --tags).pk3
 
-rm  -f $filename
-zip -R $filename "*.md" "*.txt" "*.zs"
-gzdoom $filename "$@"
+rm  -f "$filename"
+zip -R "$filename" "*.md" "*.txt" "*.zs"
+gzdoom "$filename" "$@"
